a55858481b1cd1cc6cf091e6fbafe8d7a2a43bdb
lucas yee <lucasyee999@gmail.com>
2021-11-10 23:49:08 +0800

    initial commit

---
+++ components/Layout.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Head from 'next/head';
+import { AppBar, Toolbar, Typography, Container } from '@mui/material';
+import useStyles from '../utils/styles';
+
+export default function Layout({ children }) {
+  const classes = useStyles();
+  return (
+    <div>
+      <Head>
+        <title>Next Amazons</title>
+      </Head>
+      <AppBar position="static" className={classes.navbar}>
+        <Toolbar>
+          <Typography>amazons</Typography>
+        </Toolbar>
+      </AppBar>
+      <Container className={classes.main}>{children}</Container>
+      <footer className={classes.footer}>
+        <Typography>All rights reserved, Next Amazons</Typography>
+      </footer>
+    </div>
+  );
+}
