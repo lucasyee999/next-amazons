82ceecdef7b6adc7c1e8ce710234677c77cb1efb
lucas <61584821+lucasyee999@users.noreply.github.com>
2021-11-15 22:43:32 +0800

    store

---
+++ utils/Store.js
@@ -0,0 +1,10 @@
+import { createContext, useReducer } from "react";
+
+export const Store = createContext();
+const initialState = {
+    darkMode: false
+};
+
+export function StoreProvider(props) {
+    const[state, dispatch] = [state, dispatch] = useReducer(reducer, initialState)
+}
